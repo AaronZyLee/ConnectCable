c73f4fe1d62f582491a5a3d092e5abd5bdbd6988
LZY <lizeyu0325@qq.com>
2017-07-31 10:55:58 +0800

    Initial Commit

---
+++ ConnectCable/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Autodesk.Revit.DB;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.UI;
+
+namespace ConnectCable
+{
+    [Transaction(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
+
+    
+    public class connectCable : IExternalCommand
+    {
+        Document doc;
+
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CreateAdaptiveComponentInstance(Document document, FamilySymbol symbol, List<XYZ> points)
+        {
+            // Create a new instance of an adaptive component family  
+            FamilyInstance instance = AdaptiveComponentInstanceUtils.CreateAdaptiveComponentInstance(document, symbol);
+
+            // Get the placement points of this instance  
+            IList<ElementId> placePointIds = new List<ElementId>();
+            placePointIds = AdaptiveComponentInstanceUtils.GetInstancePlacementPointElementRefIds(instance);
+
+            // Set the position of each placement point  
+            for (int i = 0; i < placePointIds.Count; i++)
+            {
+                ReferencePoint point = document.GetElement(placePointIds[i]) as ReferencePoint;
+                point.Position = points[i];
+            }
+        }
+    }
+}
